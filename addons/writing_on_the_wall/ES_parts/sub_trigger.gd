104c735974d46c8377884eab263a3a9064b24719
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2021-05-15 16:27:39 +0200

    Worked on triggers

--- addons/writing_on_the_wall/ES_parts/sub_trigger.gd
+++ addons/writing_on_the_wall/ES_parts/sub_trigger.gd
@@ -1,14 +1,12 @@
 extends Spatial
 
 func _on_area_body_entered(body):
-	if body.name == 'Player':
-		SubManager.textTriggerNr += 1
-		get_parent().get_parent().showText()
-		print('area entered')
+	SubManager.current_line += 1
+	get_parent().get_parent().showText()
+	print('Trigger: area entered')
 
 
 func _on_area_body_exited(body):
-	if body.name == 'Player':
-		get_parent().get_parent().removeText()
-		get_node("area/col").disabled = true
-		print('area left')
+	get_parent().get_parent().removeText()
+	get_node("area/col").disabled = true
+	print('Trigger: exited area')
