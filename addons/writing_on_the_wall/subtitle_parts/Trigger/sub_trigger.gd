a109688cd67006f94ef90eade7e0cad0a9b8c64b
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2021-06-01 13:16:17 +0200

    TextTriggers can now be left active if wanted

--- addons/writing_on_the_wall/subtitle_parts/Trigger/sub_trigger.gd
+++ addons/writing_on_the_wall/subtitle_parts/Trigger/sub_trigger.gd
@@ -1,7 +1,13 @@
 extends Spatial
 
+export var disable_on_leave: bool
+
 func _on_area_body_entered(body):
 #	Check if body entered is the player and if the text file has a next line
+	if body.name != 'Player':
+		print("Warning: TextTrigger is colliding with a body that is not Player")
+	if body.name != 'Player':
+		print("Warning: TextTrigger is colliding with a body that is not Player")
 	if body.name == 'Player' && String(SubManager.current_line + 1) in SubManager.subtitle_data.text:
 		SubManager.current_line += 1
 		SubManager.next_line()
@@ -11,6 +17,7 @@ func _on_area_body_entered(body):
 
 
 func _on_area_body_exited(body):
-	if body.name == 'Player':
+	if body.name == 'Player' && disable_on_leave:
 		get_node("area/col").disabled = true
+	else:
 		print('Trigger: exited area')
