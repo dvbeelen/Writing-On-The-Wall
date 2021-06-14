345b6105591a57d37130db6991d81c1cdf4c8a5b
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2021-06-14 08:25:37 +0200

    Made triggering body on triggers changable

--- addons/writing_on_the_wall/subtitle_parts/Trigger/sub_trigger.gd
+++ addons/writing_on_the_wall/subtitle_parts/Trigger/sub_trigger.gd
@@ -1,21 +1,22 @@
 extends Spatial
 
 export var disable_on_leave: bool
+export var triggering_body_name: String = 'Player'
 
 func _on_area_body_entered(body):
 #	Check if body entered is the player and if the text file has a next line
-	if body.name == 'Player' && String(SubManager.current_line + 1) in SubManager.subtitle_data.text:
+	if body.name == triggering_body_name && String(SubManager.current_line + 1) in SubManager.subtitle_data.text:
 		SubManager.current_line += 1
 		SubManager.next_line()
 		print('Trigger: area entered')
 	else:
 		print('Warning: Reached the last displayable line')
-	if body.name != 'Player':
+	if body.name != triggering_body_name:
 		print("Warning: TextTrigger is colliding with a body that is not Player")
 
 
 func _on_area_body_exited(body):
-	if body.name == 'Player' && disable_on_leave:
+	if body.name == triggering_body_name && disable_on_leave:
 		get_node("area/col").disabled = true
 	else:
 		print('Trigger: exited area')
